0a3271f092f18e2263b85d9f410a19b7f9df0b28
em-cloudsec <eimiller123@yahoo.com>
2025-11-28 18:32:43 -0800

    Create variables.tf for incident response project
    Added a placeholder for incident response project variables.

---
+++ 04-incident-response-ec2-quarantine/terraform/variables.tf
@@ -0,0 +1,8 @@
+// variables.tf - placeholder for incident response project variables.
+//
+// Example variables you might add later:
+//   - quarantine_sg_name
+//   - vpc_id
+//   - ssm_automation_document_name
+//
+// Keeping this file now as part of the Terraform project structure.
