2d6519789654f9e790d103e235d94bd461322f4d
em-cloudsec <eimiller123@yahoo.com>
2025-11-28 18:29:24 -0800

    Create outputs.tf

---
+++ 03-cloudtrail-securityhub-monitoring/terraform/outputs.tf
@@ -0,0 +1,8 @@
+// outputs.tf - placeholder for monitoring resource outputs.
+//
+// Example outputs you might add later:
+//   - cloudtrail_trail_arn
+//   - log_bucket_arn
+//   - security_hub_status
+//
+// This file will help expose important ARNs and IDs after Terraform apply.
