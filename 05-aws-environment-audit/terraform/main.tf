e5d2b5f5f399ac9643f4e9d2bffa3fef5a2df52b
em-cloudsec <eimiller123@yahoo.com>
2025-11-28 18:40:09 -0800

    Create main.tf for AWS Environment Security Audit
    Added a placeholder main.tf for AWS Environment Security Audit project.

---
+++ 05-aws-environment-audit/terraform/main.tf
@@ -0,0 +1,9 @@
+// main.tf - placeholder for AWS Environment Security Audit project.
+//
+// In the future, this file can define a small test environment to audit, such as:
+// - VPC with a few subnets
+// - A couple of security groups
+// - One or two EC2 instances
+//
+// Alternatively, it can be used solely for data sources that read existing
+// infrastructure for reporting purposes.
