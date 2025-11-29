97f4b6b6fcd59b84fd2792dc85a82740dd465956
em-cloudsec <eimiller123@yahoo.com>
2025-11-28 18:40:23 -0800

    Create variables.tf for environment audit variables
    Added a placeholder file for environment audit variables in Terraform.

---
+++ 05-aws-environment-audit/terraform/variables.tf
@@ -0,0 +1,8 @@
+// variables.tf - placeholder for environment audit variables.
+//
+// Example variables you might add later:
+//   - target_vpc_id
+//   - include_ec2
+//   - include_iam
+//
+// Keeping this file now as part of the Terraform project structure.
