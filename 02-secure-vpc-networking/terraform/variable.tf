81c4b40241110d1afa6836d7416ac74b6e6cfe38
em-cloudsec <eimiller123@yahoo.com>
2025-11-28 18:24:47 -0800

    Clarify variable.tf comments for VPC project
    Updated comments in variable.tf to clarify future variables.

--- 02-secure-vpc-networking/terraform/variable.tf
+++ 02-secure-vpc-networking/terraform/variable.tf
@@ -1,2 +1,9 @@
-// variables.tf placeholder for VPC project variables.
-// Will include VPC CIDR, subnet CIDRs, and availability zones later.
+// variables.tf - placeholder for VPC project variables.
+//
+// Example variables you might add later:
+//   - vpc_cidr
+//   - public_subnet_cidrs
+//   - private_subnet_cidrs
+//   - availability_zones
+//
+// Keeping this file now as part of the Terraform project structure.
