77cccc67d8b0e2cbb509d8336fbabd7d4abc1a8c
em-cloudsec <eimiller123@yahoo.com>
2025-11-28 18:29:11 -0800

    Create variables.tf for monitoring project
    Add placeholder for monitoring project variables.

---
+++ 03-cloudtrail-securityhub-monitoring/terraform/variables.tf
@@ -0,0 +1,8 @@
+// variables.tf - placeholder for monitoring project variables.
+//
+// Example variables you might add later:
+//   - log_bucket_name
+//   - enable_multi_region_trail
+//   - security_hub_standards_enabled (CIS, FSBP, etc.)
+//
+// Keeping this file now as part of the Terraform project structure.
